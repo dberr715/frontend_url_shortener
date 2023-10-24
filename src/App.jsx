db4fc5adec6727da33e74f5fc8c886e7d3d63f37
David Berry <dberry3755@gmail.com>
2023-10-24 14:57:41 -0400

    css and front end complete

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,14 @@ function App() {
         class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
         width="1308"
       />
+
+      <h1 class="flex items-center text-5xl font-extrabold dark:text-white self-center ">
+        Url Shrink Ray
+        <span class=" bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">
+          PRO
+        </span>
+      </h1>
+
       <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div class="mx-auto max-w-md">
